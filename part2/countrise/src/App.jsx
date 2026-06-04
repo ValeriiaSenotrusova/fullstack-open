2ccee2c374e5e37ae6d5d86e697552be35ff12d5
Lera <senotrusovavaleriia@gmail.com>
2026-06-04 14:03:40 +0300

    2.18

---
+++ part2/countrise/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState, useEffect } from 'react'
+import countriseService from './services/countrise'
+import Filter from './components/Filter'
+
+
+const App = () => {
+  const[countrise, setCountries] = useState([])
+  const[search, setSearch] = useState('')
+  
+
+  useEffect(() => {
+    countriseService
+      .getAll()
+      .then(data=>setCountries(data))
+
+  },[])
+
+  const countriesToFind = search === ''
+  ? countrise
+  : countrise.filter(country=>
+    country.name.common.toLowerCase().includes(search.toLowerCase())
+  )
+
+
+  const handleChangeSearch=(event)=> setSearch(event.target.value)
+  
+  
+  return (
+    <div>
+      <Filter value={search} onChange={handleChangeSearch}></Filter>
+
+      {search !=='' &&(
+        countriesToFind.length > 10
+        ? <p>Too many matches, specify another filter</p>
+        : countriesToFind.length === 1 
+          ? <div>
+            <h1>{countriesToFind[0].name.common}</h1>
+            <p>Capital {countriesToFind[0].capital}</p>
+            <p>Area {countriesToFind[0].area}</p>
+            <h2>Languages</h2>
+            
+            <ul>
+              {Object.values(countriesToFind[0].languages).map(lang => <li>{lang}</li> )}
+            </ul>
+            <img src={countriesToFind[0].flags.png} alt={countriesToFind[0].name.common} width={200}/>
+
+
+          </div>
+          : <ul>
+            {countriesToFind.map(country=>
+              <li key={country.name.common}>{country.name.common}</li>
+            )}
+          </ul>
+
+
+      )}
+    </div>
+  )
+
+  
+}
+
+export default App
---
+++ part2/countrise/src/components/Filter.jsx
@@ -0,0 +1,9 @@
+const Filter = ({value, onChange})=>{
+  return(
+    <div>
+      find countrise <input value={value} onChange={onChange}></input>
+    </div>
+  )
+
+}
+export default Filter
