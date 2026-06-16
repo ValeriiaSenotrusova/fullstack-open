5c98c80b51fa07775ab57ec3dbbd3de1ce0c3b61
Lera <senotrusovavaleriia@gmail.com>
2026-06-16 21:59:23 +0300

    added dist and midelwear

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -6,6 +6,8 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
+
 
 morgan.token('body',(request)=> {
   return JSON.stringify(request.body)
