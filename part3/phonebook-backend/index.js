038e0d3f6abd1bdd5dcb29ca9132577f72951390
Lera <senotrusovavaleriia@gmail.com>
2026-06-16 15:03:27 +0300

    for render

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
-
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -87,8 +88,8 @@ app.post('/api/persons',(request,response)=>{
   
 })
 
-const PORT = 3001
+const PORT = process.evn.PORT || 3001
 
 app.listen(PORT,()=> {
-  console.log('Ok')
+  console.log(`Server running on port ${PORT}`)
 })
